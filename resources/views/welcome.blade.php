6ed3b9d3fc342e753cb51a13e880f3136d5b97fd
sebbn <sebastian.westlinder@gmail.com>
2017-11-24 14:34:23 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LABEL MAKER
                 </div>
 
                 <div class="links">
